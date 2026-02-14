8df98dce348ab627bb367a7b8866d13b06a91fa5
Vladimir_Voronin <v.voronin0204@gmail.com>
2026-02-15 02:29:07 +0700

    Initial commit with first version of word counter programm

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using word_counter;
+using static System.Int32;
+
+FolderPath:
+Console.WriteLine("Enter folder path");
+var folderPath = Console.ReadLine();
+
+if (string.IsNullOrEmpty(folderPath))
+{
+    Console.WriteLine("Directory does not exist.");
+    goto FolderPath;
+}
+
+WordLength:
+Console.WriteLine("Enter min word length");
+var isNumber = TryParse(Console.ReadLine(), out var minWordLength);
+
+if (!isNumber || minWordLength < 1)
+{
+    Console.WriteLine("Invalid word length.");
+    goto WordLength;
+}
+
+var topWords = WordCounter.Count(folderPath, minWordLength);
+
+Console.WriteLine("\nTop 10 words:\n");
+
+foreach (var (word, value) in topWords)
+{
+    Console.WriteLine($"{word} - {value}");
+}
---
+++ WordCounter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace word_counter
+{
+    public static class WordCounter
+    {
+        public static IEnumerable<KeyValuePair<string, int>> Count(string folderPath, int minWordLength)
+        {
+            var wordCounts = new ConcurrentDictionary<string, int>(
+                StringComparer.OrdinalIgnoreCase);
+
+            var files = Directory.EnumerateFiles(folderPath, "*.txt",
+                SearchOption.TopDirectoryOnly);
+
+            Parallel.ForEach(files, file => { ProcessFile(file, wordCounts, minWordLength); });
+
+            return wordCounts
+                .OrderByDescending(kvp => kvp.Value)
+                .Take(10);
+        }
+
+        private static void ProcessFile(string file, ConcurrentDictionary<string, int> wordCounts,
+            int minWordLength)
+        {
+            try
+            {
+                foreach (var line in File.ReadLines(file))
+                {
+                    foreach (var word in ExtractWords(line, minWordLength))
+                    {
+                        wordCounts.AddOrUpdate(word, 1, (_, oldValue) => oldValue + 1);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing {file}: {ex.Message}");
+            }
+        }
+
+        private static IEnumerable<string> ExtractWords(string text, int minLength)
+        {
+            foreach (Match match in Regex.Matches(text, @"\b[\p{L}]+\b"))
+            {
+                var word = match.Value.ToLowerInvariant();
+                if (word.Length > minLength)
+                {
+                    yield return word;
+                }
+            }
+        }
+    }
+}
